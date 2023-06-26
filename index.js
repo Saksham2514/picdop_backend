2b58f214771464e4a273260a63332eb493fb3ec9
Saksham Goyal <saksham069@gmail.com>
2023-06-26 18:21:25 +0530

    Final

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json({limit:"100MB"}));
 app.use(express.urlencoded({extended:true}));
-app.use(cors())
+app.use(cors());
 
 const router = require("./router");
 
