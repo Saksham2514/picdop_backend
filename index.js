cc3a8a6d308a38f60db955f22578d97c9b15da8f
Saksham Goyal <saksham069@gmail.com>
2023-07-28 23:31:26 +0530

    NEW

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ const app = express();
 app.use(express.json({limit:"100MB"}));
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const router = require("./router");
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(router);
 
 
