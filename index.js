09e2b1958476697795219d6783d0eb319ba24b89
Saksham Goyal <saksham069@gmail.com>
2023-07-29 03:55:10 +0530

    updated

--- index.js
+++ index.js
@@ -30,7 +30,17 @@ mongoose
   const corsOptions ={
       origin:'http://localhost:3000', 
       credentials:true,            //access-control-allow-credentials:true
-      optionSuccessStatus:200
+      optionSuccessStatus:200,
+      methods: [
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE',
+      ],
+      allowedHeaders: [
+        'Content-Type',
+      ],
+    
   }
   app.use(cors(corsOptions));
 
