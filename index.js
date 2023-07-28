14992d7e728ceffa2d0621b78ae4608c5e46c93a
Saksham Goyal <saksham069@gmail.com>
2023-07-29 03:53:18 +0530

    updated

--- index.js
+++ index.js
@@ -7,16 +7,8 @@ require("dotenv").config();
 const app = express();
 app.use(express.json({ limit: "100MB" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "*", credentials: true }));
-// app.use(function (req, res, next) {
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
-app.options('*',cors())
+
 const router = require("./router");
 
 const PORT = process.env.PORT || 5000;
@@ -33,6 +25,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+
+  const corsOptions ={
+      origin:'http://localhost:3000', 
+      credentials:true,            //access-control-allow-credentials:true
+      optionSuccessStatus:200
+  }
+  app.use(cors(corsOptions));
 
 app.listen(PORT, async () => {
   console.log(`server up on port ${PORT}`);
