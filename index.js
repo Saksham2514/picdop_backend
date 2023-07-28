7e792cad07025fcb559d3ed481ec3ebde27f943a
Saksham Goyal <saksham069@gmail.com>
2023-07-29 03:30:36 +0530

    updated

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors =  require("cors");
-const path =  require("path");
+const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
-
 const app = express();
-app.use(express.json({limit:"100MB"}));
-app.use(express.urlencoded({extended:true}));
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(express.json({ limit: "100MB" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*", credentials: true }));
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 const router = require("./router");
 
-
 const PORT = process.env.PORT || 5000;
 app.use(router);
 
-
-
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
